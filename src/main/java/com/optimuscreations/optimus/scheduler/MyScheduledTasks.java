7c5cc42429911a1ff78e9af99b556a9ad40e3941
Shubham Taware <tawareshubham27@gmail.com>
2025-10-06 22:50:30 +0530

    scheduling added

---
+++ src/main/java/com/optimuscreations/optimus/scheduler/MyScheduledTasks.java
@@ -0,0 +1,21 @@
+package com.optimuscreations.optimus.scheduler;
+
+import com.optimuscreations.optimus.service.ProductService;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyScheduledTasks {
+
+    private final ProductService productService;
+
+    MyScheduledTasks(ProductService productService) {
+        this.productService = productService;
+    }
+    
+    // Cron expression: second, minute, hour, day of month, month, day of week
+    @Scheduled(cron = "0 * * * * ?")  // Every day at 9:00 AM
+    public void dailyTask() {
+        System.out.println("Product inventory check executed at"+System.currentTimeMillis()+ "\n"+ productService.getAllProducts());
+    }
+}
